db2942efaf5a8b58fbe6054b2cfcb893be4007ea
ed <edlang114@gmail.com>
2024-02-24 20:26:55 -0500

    added welcome page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import { Header } from '@/app/Components/Header'
-import { TextField } from '@/app/Components/TextField'
 
 export default function Home() {
     return (
@@ -9,22 +8,14 @@ export default function Home() {
                     <div className="mx-auto max-w-7xl px-6 lg:px-8">
                         <div className="mx-auto max-w-2xl text-center">
                             <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-                                Flight Database
+                                Welcome to Group 6's Flights Queries Visualization. Click a link in the header to get started.
                             </h1>
-                        </div>
-
-                        <div className="mt-10 flex items-center justify-center gap-x-6">
-                            <TextField/>
-                            <button
-                               className="rounded-md bg-gray-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-gray-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-600">
-                                Submit <span aria-hidden="true">→</span>
-                            </button>
                         </div>
                         <div className="mt-16 flow-root sm:mt-24">
                             <div
                                 className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 md:-m-4 md:rounded-2xl lg:p-4">
                                 <img
-                                    src="https://dist.neo4j.com/wp-content/uploads/20210210101155/1Rf98vgvcLle1SZvLmmIwaQ.png"
+                                    src="https://assets.toptal.io/images?url=https%3A%2F%2Fbs-uploads.toptal.io%2Fblackfish-uploads%2Fcomponents%2Fblog_post_page%2Fcontent%2Fcover_image_file%2Fcover_image%2F1282568%2Fregular_1708x683_0712-Bad_Practices_in_Database_Design_-_Are_You_Making_These_Mistakes_Dan_Newsletter-f90d29e5d2384eab9f4f76a0a18fa9a8.png"
                                     alt=""
                                     className="rounded-md shadow-2xl ring-1 ring-gray-900/10"
                                 />
